68590c2e5232b5b24c32cca17882526ddf095803
Chetan Sarva <chetan@pixelcop.net>
2021-09-30 11:36:27 -0400

    build: updated using latest typescript template (using ncc)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import path from 'path';
 
 async function run() {
   const script = path.normalize(path.join(__dirname, '..', 'src', 'git-restore-mtime-bare'));
-  // core.info("running script " + script);
   await exec.exec('python', [script]);
 }
 
